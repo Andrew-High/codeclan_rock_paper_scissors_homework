14f2b044e6f1307e1c1906ec86a1c53e34353479
Andrew High <andrewhigh4@hotmail.com>
2021-04-16 15:52:44 +0100

    set up controller and gave routes for game and ability to input player moves into url

---
+++ controllers/controller.py
@@ -0,0 +1,15 @@
+from flask import render_template, request
+from app import app
+from models.game import Game
+from models.player import Player
+
+@app.route("/game")
+def index():
+    return "Welcome to Rock Paper Scissors!"
+
+@app.route("/game/<player1move>/<player2move>")
+def play_game(player1move, player2move):
+    player1 = Player("Player 1", player1move)
+    player2 = Player("Player 2", player2move)
+    game = Game(player1, player2)
+    return game.play_game()
