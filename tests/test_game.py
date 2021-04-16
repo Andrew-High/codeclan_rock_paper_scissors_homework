bc457a8a0695ba968a2ace8897c9e8d3410a887c
Andrew High <andrewhigh4@hotmail.com>
2021-04-16 15:02:22 +0100

    added tests for paper wins and rock wins

--- tests/test_game.py
+++ tests/test_game.py
@@ -14,5 +14,11 @@ class TestGame(unittest.TestCase):
         self.assertEqual("Andrew", self.game.player_1.name)
         self.assertEqual("Simon", self.game.player_2.name)
 
-    def test_scissors_wins(self):
+    def test_scissors_wins_against_paper(self):
         self.assertEqual("Player 1 wins!", self.game.play_game(self.player_1, self.player_2))
+
+    def test_paper_wins_against_rock(self):
+        self.assertEqual("Player 2 wins!", self.game.play_game(self.player_3, self.player_2))
+
+    def test_rock_wins_against_scissors(self):
+        self.assertEqual("Player 1 wins!", self.game.play_game(self.player_3, self.player_1))
