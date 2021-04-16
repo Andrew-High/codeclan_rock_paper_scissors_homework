00fc284cc3edfd8bdaf649d0b84db86252d159e1
Andrew High <andrewhigh4@hotmail.com>
2021-04-16 17:17:22 +0100

    added computer random gesture functionality but urls not working yet

--- controllers/controller.py
+++ controllers/controller.py
@@ -5,7 +5,7 @@ from models.player import Player
 
 @app.route("/game")
 def index():
-    return render_template("index.html", result = "Welcome to my Rock Paper Scissors game!")
+    return render_template("index.html", result = "Play a game to generate a result")
 
 @app.route("/game/<player1move>/<player2move>")
 def play_game(player1move, player2move):
@@ -19,6 +19,22 @@ def play_game(player1move, player2move):
 def play_game_with_inputs():
     player_1 = Player(request.form["player1name"], request.form["player1move"])
     player_2 = Player(request.form["player2name"], request.form["player2move"])
+    game = Game(player_1, player_2)
+    result = game.play_game()
+    return render_template("index.html", result = result)
+
+@app.route("/game", methods=["POST"])
+def play_game_with_computer():
+    player_1 = Player(request.form["player1name"], request.form["player1move"])
+    def computer_gesture():
+        random_number = random()
+        if random_number >= 0 and random_number < 0.33:
+            return "rock"
+        elif random_number >= 0.33 and random_number < 0.66:
+            return "paper"
+        elif random_number >= 0.66:
+            return "scissors"
+    player_2 = Player("The computer", computer_gesture)
     game = Game(player_1, player_2)
     result = game.play_game()
     return render_template("index.html", result = result)
