7209f18a686336bfe7d2686755aa5d5c2abe8352
Andrew High <andrewhigh4@hotmail.com>
2021-04-16 15:24:29 +0100

    working winning game tests

--- models/game.py
+++ models/game.py
@@ -2,3 +2,23 @@ class Game:
     def __init__(self, player_1, player_2):
         self.player_1 = player_1
         self.player_2 = player_2
+
+    def play_game(self):
+        if self.player_1.move == "rock" and self.player_2.move == "scissors":
+            return "Player 1 wins!"
+        elif self.player_1.move == "rock" and self.player_2.move == "paper":
+            return "Player 2 wins!"
+        elif self.player_1.move == "scissors" and self.player_2.move == "paper":
+            return "Player 1 wins!"
+        elif self.player_1.move == "scissors" and self.player_2.move == "rock":
+            return "Player 2 wins!"
+        elif self.player_1.move == "paper" and self.player_2.move == "rock":
+            return "Player 1 wins!"
+        elif self.player_1.move == "paper" and self.player_2.move == "scissors":
+            return "Player 2 wins!"
+        elif self.player_1.move == self.player_2.move:
+            return "It's a draw!"
+        else:
+            return "Please input a valid game"
+        
+            
--- tests/test_game.py
+++ tests/test_game.py
@@ -8,17 +8,19 @@ class TestGame(unittest.TestCase):
         self.player_1 = Player("Andrew", "scissors")
         self.player_2 = Player("Simon", "paper")
         self.player_3 = Player("Richard", "rock")
-        self.game = Game(self.player_1, self.player_2)
+        self.game_1 = Game(self.player_1, self.player_2)
+        self.game_2 = Game(self.player_3, self.player_2)
+        self.game_3 = Game(self.player_3, self.player_1)
     
     def test_game_has_players(self):
-        self.assertEqual("Andrew", self.game.player_1.name)
-        self.assertEqual("Simon", self.game.player_2.name)
+        self.assertEqual("Andrew", self.game_1.player_1.name)
+        self.assertEqual("Simon", self.game_1.player_2.name)
 
     def test_scissors_wins_against_paper(self):
-        self.assertEqual("Player 1 wins!", self.game.play_game(self.player_1, self.player_2))
+        self.assertEqual("Player 1 wins!", self.game_1.play_game())
 
     def test_paper_wins_against_rock(self):
-        self.assertEqual("Player 2 wins!", self.game.play_game(self.player_3, self.player_2))
+        self.assertEqual("Player 2 wins!", self.game_2.play_game())
 
     def test_rock_wins_against_scissors(self):
-        self.assertEqual("Player 1 wins!", self.game.play_game(self.player_3, self.player_1))
+        self.assertEqual("Player 1 wins!", self.game_3.play_game())
