47c258464d2b8e6e8882501a0c3362de5e236d5c
Andrew High <andrewhigh4@hotmail.com>
2021-04-17 11:28:07 +0100

    working single player mode

--- controllers/controller.py
+++ controllers/controller.py
@@ -2,12 +2,13 @@ from flask import render_template, request
 from app import app
 from models.game import Game
 from models.player import Player
+from random import random
 
 @app.route("/game")
 def index():
-    return render_template("index.html", result = "Play a game to generate a result")
+    return render_template("index.html")
 
-@app.route("/game/<player1move>/<player2move>")
+@app.route("/game/two_player/<player1move>/<player2move>")
 def play_game(player1move, player2move):
     player_1 = Player("Player 1", player1move)
     player_2 = Player("Player 2", player2move)
@@ -15,15 +16,24 @@ def play_game(player1move, player2move):
     result = game.play_game()
     return render_template("index.html", result = result)
 
-@app.route("/game", methods=["POST"])
+@app.route("/game/two_player")
+def two_player_homepage():
+    return render_template("two_player.html", result = "Play a game to get a result")
+
+@app.route("/game/two_player", methods=["POST"])
 def play_game_with_inputs():
     player_1 = Player(request.form["player1name"], request.form["player1move"])
     player_2 = Player(request.form["player2name"], request.form["player2move"])
     game = Game(player_1, player_2)
     result = game.play_game()
-    return render_template("index.html", result = result)
+    return render_template("two_player.html", result = result)
 
-@app.route("/game", methods=["POST"])
+@app.route("/game/single_player", methods=["GET"])
+def single_player_homepage():
+    return render_template("single_player.html", result = "Play a game to get a result")
+
+
+@app.route("/game/single_player", methods=["POST"])
 def play_game_with_computer():
     player_1 = Player(request.form["player1name"], request.form["player1move"])
     def computer_gesture():
@@ -34,8 +44,8 @@ def play_game_with_computer():
             return "paper"
         elif random_number >= 0.66:
             return "scissors"
-    player_2 = Player("The computer", computer_gesture)
+    player_2 = Player("Computer", computer_gesture())
     game = Game(player_1, player_2)
     result = game.play_game()
-    return render_template("index.html", result = result)
+    return render_template("single_player.html", result = result)
     
--- models/game.py
+++ models/game.py
@@ -5,17 +5,17 @@ class Game:
 
     def play_game(self):
         if self.player_1.move == "rock" and self.player_2.move == "scissors":
-            return f"{self.player_1.name} wins by playing rock!"
+            return f"{self.player_1.name} wins by playing rock, beating {self.player_2.name}'s scissors!!"
         elif self.player_1.move == "rock" and self.player_2.move == "paper":
-            return f"{self.player_2.name} wins by playing paper!"
+            return f"{self.player_2.name} wins by playing paper, beating {self.player_1.name}'s rock!!"
         elif self.player_1.move == "scissors" and self.player_2.move == "paper":
-            return f"{self.player_1.name} wins by playing scissors!"
+            return f"{self.player_1.name} wins by playing scissors, beating {self.player_2.name}'s paper!!"
         elif self.player_1.move == "scissors" and self.player_2.move == "rock":
-            return f"{self.player_2.name} wins by playing rock!"
+            return f"{self.player_2.name} wins by playing rock, beating {self.player_1.name}'s scissors!!"
         elif self.player_1.move == "paper" and self.player_2.move == "rock":
-            return f"{self.player_1.name} wins by playing paper!"
+            return f"{self.player_1.name} wins by playing paper, beating {self.player_2.name}'s rock!!"
         elif self.player_1.move == "paper" and self.player_2.move == "scissors":
-            return f"{self.player_2.name} wins by playing scissors!"
+            return f"{self.player_2.name} wins by playing scissors, beating {self.player_1.name}'s paper!"
         elif self.player_1.move == self.player_2.move:
             return "It's a draw!"
         else:
